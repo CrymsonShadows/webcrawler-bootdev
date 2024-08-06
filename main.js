de41f95903f2bd9f4e7bc89efca701d32bbf40dc
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2024-08-06 16:23:04 -0500

    exit if incorrect amount of arguments given and store baseURL for later if given

--- main.js
+++ main.js
@@ -3,10 +3,13 @@ import { argv } from 'node:process';
 function main() {
     if (argv.length < 3) {
         console.log("No website url given to crawl.");
+        return;
     } else if (argv.length > 3) {
         console.log("Too many arguments given. Only give the website url to crawl.");
+        return;
     } else {
-        console.log(`The base url to crawl will be ${argv[2]}`);
+        const baseURL = argv[2];
+        console.log(`The base url to crawl will be ${baseURL}`);
     }
 }
 
