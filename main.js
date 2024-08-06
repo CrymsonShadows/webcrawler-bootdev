ea69af0f40eb1de61489c7dc1dff3a6bcd5a2d98
Rahman Khandakar <74515048+CrymsonShadows@users.noreply.github.com>
2024-08-06 16:19:18 -0500

    add basic stub in main.js to read arguments from cli and print message if too little, many, or just one argument is given

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
-import { JSDOM } from 'jsdom'
-const urlObj = new URL('https://google.com', 'https://example.com');
-console.log(urlObj.href)
+import { argv } from 'node:process';
 
+function main() {
+    if (argv.length < 3) {
+        console.log("No website url given to crawl.");
+    } else if (argv.length > 3) {
+        console.log("Too many arguments given. Only give the website url to crawl.");
+    } else {
+        console.log(`The base url to crawl will be ${argv[2]}`);
+    }
+}
+
+main();
